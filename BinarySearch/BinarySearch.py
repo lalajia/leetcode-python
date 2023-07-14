5f558f82a9e809cd1e6894df4789b403b3d988af
Selena <selenazheng3@gmail.com>
2023-07-14 00:39:22 -0700

    Create BinarySearch.py

---
+++ BinarySearch/BinarySearch.py
@@ -0,0 +1,30 @@
+"""
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+"""
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            # (5-0)/2 = 2
+            mid = left + (right - left) // 2
+            # print(mid)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return 0
+
+
+print(Solution().search(nums=[-1, 0, 3, 5, 9, 12], target=9))
