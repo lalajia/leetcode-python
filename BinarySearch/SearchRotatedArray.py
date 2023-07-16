d2bf6eb0ddf4f3e01c535c4fadfd4901ca8e6dd7
Selena <selenazheng3@gmail.com>
2023-07-15 23:29:30 -0700

    Create SearchRotatedArray.py

---
+++ BinarySearch/SearchRotatedArray.py
@@ -0,0 +1,35 @@
+"""
+There is an integer array nums sorted in ascending order (with distinct values).
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+You must write an algorithm with O(log n) runtime complexity.
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+"""
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                # 把mid的事情交给第一个if
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[mid] < nums[right]:
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
+
+
+print(Solution().search(nums=[5, 1, 3], target=1))
